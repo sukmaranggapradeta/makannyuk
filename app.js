7b29749cf61a8370747d75622aec49a5f65e3535
sukmaranggapradeta <adit.netral@gmail.com>
2019-03-27 17:43:53 +0700

    login done

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 3006
+const bodyParser = require('body-parser')
+const multer = require('multer')
+const path = require('path')
+const ejs = require('ejs')
+//menggunakan library pada express
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+app.use(express.static(__dirname + '/public')); 
 
 
 app.use('/',require('./routes/home'))
-
+app.use('/login',require('./routes/login'))
+app.use('/signup',require('./routes/signup'))
+app.use('/addfood',require('./routes/addfood'))
 
 app.listen(port, ()=> console.log(`server running in port ${port}`))
---
+++ routes/signup.js
@@ -0,0 +1,27 @@
+const Router = require('express').Router()
+const Model = require('../models')
+
+Router.get('/',(req,res)=>{
+    res.render('./signup.ejs')
+})
+
+Router.post('/',(req,res)=>{
+    let input = req.body
+    Model.User.create({
+        first_name: input.first_name,
+        last_name: input.last_name,
+        gender: input.gender,
+        email: input.email,
+        user_name: input.user_name,
+        password: input.password,
+        createdAt:new Date,
+        updatedAt:new Date
+    })
+    .then(sukses=>{
+        res.send("Sukses login")
+    })
+    .catch(err=>{
+        res.send(err.message)
+    })
+})
+module.exports = Router
